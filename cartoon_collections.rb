29d5f64fa696f14733c58ab9eb2eea5dbaec3402
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 06:43:28 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,9 @@ def summon_captain_planet (array)
   
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls (array)
+  array.all
+  
 end
 
 def find_the_cheese# code an argument here
