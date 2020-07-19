2c8a06d848026578f5c1af233c8a9709df666dea
Miguel Felix <mikeyfelix99@gmail.com>
2020-07-19 06:42:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,11 +6,12 @@ def roll_call_dwarves (array)
 end
 
 def summon_captain_planet (array)
+  new_array = []
   array.map do |word|
-    puts "#{word.capitalize}!"
+    new_array << "#{word.capitalize}!"
   end
-  array
-  binding.pry
+  new_array
+  
 end
 
 def long_planeteer_calls# code an argument here
